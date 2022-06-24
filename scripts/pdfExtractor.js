8e39fab7ec735734ec80ed72f26d4c096f1728c0
rwanoux <erwan_lemaire@yahoo.fr>
2022-06-24 15:05:26 +0200

    folders and chapters OK !

--- scripts/pdfExtractor.js
+++ scripts/pdfExtractor.js
@@ -32,15 +32,15 @@ export default class PDFExtractor extends FormApplication {
 
     async _updateObject(event, formData) {
         const data = {
-            activePage:this.activePage,
-            maxPage:this.maxPage,
-            pdfUrl:this.pdfUrl,
-            contents:this.contents,
-            sizes:this.sizes,
-            fonts:this.fonts
+            activePage: this.activePage,
+            maxPage: this.maxPage,
+            pdfUrl: this.pdfUrl,
+            contents: this.contents,
+            sizes: this.sizes,
+            fonts: this.fonts
         };
         await game.settings.set('pdfExtractor', 'pdfExtractor', data);
-        
+
     }
     async activateListeners(html) {
 
@@ -51,6 +51,8 @@ export default class PDFExtractor extends FormApplication {
 
         let scanPage = html.find("#scanPage")[0];
         scanPage.addEventListener("click", this.scanPage.bind(this));
+        let createBut = html.find("#createJournal")[0];
+        createBut.addEventListener("click", this.createJournal.bind(this));
 
         let pageInput = html.find("#activePage")[0];
         pageInput.addEventListener("change", this.changePage.bind(this));
@@ -74,6 +76,7 @@ export default class PDFExtractor extends FormApplication {
             await this.renderPdf(this.object.pdfUrl);
         }
 
+
     }
 
     getSelection(ev) {
@@ -83,13 +86,62 @@ export default class PDFExtractor extends FormApplication {
 
 
     }
+    async createJournal(ev) {
+        for (let i = 0; i < this.contents.length - 1; i++) {
+            let cont = this.contents[i];
 
+            if (cont.role == "chapter") {
+                //if multi line chapter name : updating folder and journal with concat
+                if (this.contents[i - 1]?.role == "chapter") {
+                    console.log(this.contents[i - 1].str, cont)
+                    let oldName = game.journal.filter(j => j.name.includes(this.contents[i - 1].str))[0].name;
+                    let newName = oldName + " " + cont.str;
+
+                    await game.journal.getName(oldName).update({
+                        name: newName
+                    });
+                    await game.folders.getName(oldName).update({
+                        name: newName
+                    });
+                }
+                // else =>first line chapter label : creating journal and folder
+                else {
+                    let jFolder = ui.journal.folders.filter(f => f.name == cont.str)[0];
+                    if (!jFolder) {
+                        jFolder = await Folder.create({
+                            name: cont.str,
+                            type: "JournalEntry",
+                            sort: i
+                        });
+                        let data = {
+                            "name": cont.str,
+                            "folder": jFolder.id
+                        };
+                        await JournalEntry.create(
+                            data
+                        );
+                    }
+
+                }
+            }
+            if (cont.role == "section") {
+                let previousChapter = this.contents.filter(c => c.role == "chapter" && this.contents.indexOf(c) <= this.contents.indexOf(cont))
+                let lastChapterName = game.folders.filter(f => f.name.includes(previousChapter[previousChapter.length - 1].str));
+                console.log(lastChapterName);
+
+            }
+            if (cont.role == "paragraphe") {
+
+            }
+
+        }
+    }
     async setPdfUrl(ev) {
         let obj = await game.settings.get("pdfExtractor", "pdfExtractor");
         obj.pdfUrl = document.getElementById('pdfUrl').value;
         this.pdfUrl = document.getElementById('pdfUrl').value;
-        this.activePage=1;
-        document.getElementById('activePage').value=1;
+        this.activePage = 1;
+        document.getElementById('activePage').value = 1;
         this._updateObject();
         return this.renderPdf();
 
@@ -122,10 +174,10 @@ export default class PDFExtractor extends FormApplication {
         obj.sizes = {};
         obj.pages = [];
         let responses = 0;
-        let structure=[];
+        let structure = [];
 
 
-        loadingTask.promise.then(async function(pdf) {
+        loadingTask.promise.then(async function (pdf) {
             let maxPage = pdf.numPages;
             //getting textContent by pages
             for (var j = 0; j <= maxPage - 1; j++) {
@@ -143,43 +195,44 @@ export default class PDFExtractor extends FormApplication {
                 }
                 for (let it of p.items) {
 
-                    //unique content
-                    if (it.height != 0 && p.items[p.items.indexOf(it) - 1] ?.str != it.str ) {
+                    //cleaning doubled
+
+                    if (it.height != 0 && p.items[p.items.indexOf(it) - 1]?.str != it.str) {
+
+                        // cleaning unknown charcode
                         it.str = it.str.replace(String.fromCharCode(65533), "");
                         it.numPage = i + 1;
                         obj.contents.push(it);
-                        if (!obj.sizes[it.height]) {
-                            obj.sizes[it.height] = {
+                        if (!obj.sizes[it.height.toString().replace(".", ",")]) {
+                            obj.sizes[it.height.toString().replace(".", ",")] = {
                                 tag: "truc"
                             };
                         }
                         //defining journals structure
 
                         //chapters
-                        if ((it.height==70||it.height>35)&& it.str.length>3){
-                            if (structure.filter(c=>c.numPage==it.numPage).length==0){
-                                 structure[structure.length]=new Chapter(it);
-                            }
-                           else if (structure.filter(c=>c.numPage==it.numPage).length>0){
-                            structure[structure.indexOf(structure.filter(c=>c.numPage==it.numPage)[0])].label+=" "+it.str;
-                           }
+                        if ((it.height == 70 || it.height > 35) && it.str.length > 3) {
+                            it.role = "chapter";
                         }
-                        structure=structure.sort((a,b)=>{
-                            return  a.item.numPage-b.item.numPage;
-                         });
-                         if (it.height==18 && !parseInt(it.str) &&!it.str.toLowerCase().includes("chapitre")){
-                            console.log(it)
-                            structure.filter(c=>c.numPage<=it.numPage)[structure.filter(c=>c.numPage<=it.numPage).length-1].sections.push(new Section(it))
-                          console.log( structure.filter(c=>c.numPage<=it.numPage)[structure.filter(c=>c.numPage<=it.numPage).length-1])
+                        // section
+                        if (it.height == 18 && !parseInt(it.str) && !it.str.toLowerCase().includes("chapitre")) {
+                            it.role = "section";
+                        }
+                        if (it.height == 10 || it.height == 11 || (it.height > 39 && it.height < 41)) {
+                            it.role = "paragraphe";
+                        }
+                        if (it.height == 16) {
+                            it.role = "encardLabel";
+                        }
+                        if (it.height == 15 || it.height == 14 || it.height == 12 || it.height == 9.8) {
+                            it.role = "subtitle";
+                        }
 
-                        }
-                         if (it.height==10 || (it.height>39 && it.height<41)){
-
-                        }
                     }
 
 
                 }
+                let cleanContents = obj.contents.filter((ele, pos) => obj.contents.indexOf(ele) == pos);
 
 
                 if (i == obj.pages.length - 1) {
@@ -193,9 +246,9 @@ export default class PDFExtractor extends FormApplication {
 
                 }
             }
-          
-            structure.forEach(t=>{console.log(t);});
-        }).then(this._updateObject()).then(this.render());
+
+            structure.forEach(t => { console.log(t); });
+        }).then(this._updateObject()).then(this.render()).then(console.log("______________scan done_______"));
 
 
     }
@@ -204,12 +257,12 @@ export default class PDFExtractor extends FormApplication {
 
     async renderPdf() {
         let activePage = this.activePage || 1;
-        let maxPage=0;
+        let maxPage = 0;
         var loadingTask = pdfjsLib.getDocument(this.pdfUrl);
-        await loadingTask.promise.then(async function(pdf) {
-            maxPage=pdf.numPages;
+        await loadingTask.promise.then(async function (pdf) {
+            maxPage = pdf.numPages;
 
-            pdf.getPage(activePage).then(async function(page) {
+            pdf.getPage(activePage).then(async function (page) {
                 // you var scale = 1.5;
                 var viewport = page.getViewport({ scale: 0.9 });
                 // Support HiDPI-screens.
@@ -234,8 +287,8 @@ export default class PDFExtractor extends FormApplication {
                 await page.render(renderContext);
                 let textContent = await page.getTextContent();
                 document.getElementById("text-layer").innerHTML = "";
-               
-                    // Pass the data to the method for rendering of text over the pdf canvas.
+
+                // Pass the data to the method for rendering of text over the pdf canvas.
                 pdfjsLib.renderTextLayer({
                     textContent: textContent,
                     container: $("#text-layer").get(0),
@@ -247,7 +300,7 @@ export default class PDFExtractor extends FormApplication {
             });
 
         });
-        this.maxPage=maxPage;
+        this.maxPage = maxPage;
         this._updateObject();
 
     }
@@ -255,19 +308,19 @@ export default class PDFExtractor extends FormApplication {
 
 }
 
-class Chapter{
-    constructor(it){
-        this.item=it;
-        this.label=it.str.toUpperCase();
-        this.numPage=it.numPage;
-        this.sections=[];
+class Chapter {
+    constructor(it) {
+        this.item = it;
+        this.label = it.str.toUpperCase();
+        this.numPage = it.numPage;
+        this.sections = [];
     }
 }
 
-class Section{
-    constructor(it){
-        this.item=it;
-        this.label=it.str.toUpperCase();
-        this.numPage=it.numPage;
+class Section {
+    constructor(it) {
+        this.item = it;
+        this.label = it.str.toUpperCase();
+        this.numPage = it.numPage;
     }
 }
